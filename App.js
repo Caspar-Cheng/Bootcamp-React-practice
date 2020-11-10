9f47e4dc8dcffbecf2106f70957730733fac6b6e
Caspar <52498280+Caspar-Cheng@users.noreply.github.com>
2020-11-10 20:57:34 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 // this is the component document for searching-app 
 
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash"
 import SearchBar from "./SearchBar";
 
 class App extends React.Component{
+    state = { images: []};
 
+
+    //setting async and await keyword to call back data at specific time; this way more clear and code-clean
+    onSearchSubmit = async (term) => {
+ 
+        const response = await unsplash.get("/search/photos", {
+        params: {query: term},
+        
+      })
+        this.setState({images: response.data.results});
+    };
+
+    /* Another way to get the same result is by using then() function will call back whatever data we got from the API
     onSearchSubmit(term){
-        //use axios as a 3rd party package to this app
-        //use axios get request and query the search term
         axios.get("https://api.unsplash.com/search/photos", {
         params: {query: term},
         headers:{
@@ -16,24 +27,14 @@ class App extends React.Component{
         }
         }).then((response) => {
             console.log(response)
-        })//axios return a promise and this then() function will call back whatever data we got from the API
-    };
-
-    /* Another way to get the same result is by setting async and await keyword to call back data at specific time; this way more clear and code-clean
-    async onSearchSubmit(term){
-        const response = await axios.get("https://api.unsplash.com/search/photos", {
-        params: {query: term},
-        headers:{
-            Authorization: "Client-ID E4iB0yW6uDSw2K76emKh_Og-7h-LhiSNFX8pVm5bAS8"
-        }
         })
-        console.log(response.data.results);
     }*/   
 
     render(){
         return (
             <div className="ui container" style={{marginTop: "10px"}}>
             <SearchBar onSubmit={this.onSearchSubmit}/>
+            Found: {this.state.images.length} images
             </div>
         )
     }
